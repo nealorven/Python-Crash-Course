78790ea8c8d8c312a137fdbac1a4dea84659ff92
Neal O <nealorven@gmail.com>
2019-03-19 17:53:29 +0600

    Redacted 5_if_Oterator\4_

---
+++ 1_Chapter_Basics/5_if_Operator/4_check_value_list.py
@@ -0,0 +1,17 @@
+### Иногда бывает важно проверить, содержит ли список
+### некоторое значение, прежде чем выполнять действие:
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+print('mushrooms' in requested_toppings)
+print('pepperoni' in requested_toppings)
+
+# True
+# False
+
+### Проверка отсутствия значения в списке:
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(user.title() + ", you can post a response if you wish.")
+
+# Marie, you can post a response if you wish.
