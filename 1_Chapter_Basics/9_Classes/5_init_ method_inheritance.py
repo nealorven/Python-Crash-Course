af47e7fdd2d7d4a7b8b68d937761bdd99dff1bb1
Neal O <nealorven@gmail.com>
2019-04-10 17:45:42 +0600

    Update 04 10 19 05:45

--- 1_Chapter_Basics/9_Classes/5_init_ method_inheritance.py
+++ 1_Chapter_Basics/9_Classes/5_init_ method_inheritance.py
@@ -15,12 +15,14 @@
 
 class Car:
     """Простая модель автомобиля."""
+
     def __init__(self, make, model, year):
+        # Атрибуты
         self.make = make
         self.model = model
         self.year = year
-        self.odometer_reading = 0
         self.gas_tank = 80
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         long_name = f"{self.year} {self.make} {self.model}"
@@ -29,33 +31,57 @@ class Car:
     def read_odometer(self):
         print(f"This car has {self.odometer_reading} miles on it.")
 
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
     def update_odometer(self, mileage):
         if mileage >= self.odometer_reading:
             self.odometer_reading = mileage
         else:
             print("You can't roll back an odometer!")
 
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
-
+    # Переопределение методов класса-родителя
     def fill_gas_tank(self):
         gas = self.gas_tank
         return gas
 
 
-# Имя super происходит из распространенной терминологии: класс-родитель
-# называется суперклассом, а класс-потомок — субклассом.
+class Battery:
+    """Простая модель аккумулятора электромобиля."""
+
+    def __init__(self, battery_size=68):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Выводит информацию о мощности аккумулятора."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Выводит приблизительный запас хода для аккумулятора."""
+
+        #global range_kwh
+        if self.battery_size >= 70:
+            range_kwh = 248
+        elif self.battery_size <= 85:
+            range_kwh = 274
+
+        message = f"This car can go approximately {range_kwh}"
+        message += " miles on a full charge."
+        print(message)
+
+
 class ElectricCar(Car):
     """Представляет аспекты машины, специфические для электромобилей."""
     def __init__(self, make, model, year):
         """Инициализирует атрибуты класса-родителя."""
         super().__init__(make, model, year)
-        self.battery_size = 70
+        # Добавляем атрибут с именем self.battery и создаем экземпляр Battery()
+        # Теперь любой экземпляр ElectricCar будет иметь автоматически
+        # создаваемый экземпляр Battery.
+        # Экземпляры как атрибуты
+        self.battery = Battery()
 
-    def describe_battery(self):
-        """Выводит информацию о мощности аккумулятора."""
-        print(f"This car has a {self.battery_size} -kWh battery.")
-
+    # Переопределение методов класса-родителя
     def fill_gas_tank(self):
         """У электромобилей нет бензобака."""
         print("This car doesn't need a gas tank!")
@@ -63,9 +89,11 @@ class ElectricCar(Car):
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
 my_tesla.fill_gas_tank()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
 
 # 2016 Tesla Model S
-# This car has a 70-kWh battery.
 # This car doesn't need a gas tank!
+# This car has a 70-kWh battery.
+# This car can go approximately 240 miles on a full charge.
---
+++ 1_Chapter_Basics/9_Classes/Exercises/9_8_privilege.py
@@ -0,0 +1,43 @@
+class User:
+    def __init__(self, first_name, last_name):
+        self.f_name = first_name
+        self.l_name = last_name
+        self.login_attempts = 0
+
+    def increment_login_attempts(self, attempts):
+        self.login_attempts += attempts
+
+    def reset_login_attempts(self):
+        reset_attempts = self.login_attempts = 0
+        return reset_attempts
+
+    def read_attempts(self):
+        read_att = self.login_attempts
+        return read_att
+
+
+class Privileges:
+    def __init__(self, first_name, last_name):
+        self.privileges = [
+            'allowed to add messages',
+            'allowed to delete users',
+            'allowed to ban users'
+            ]
+
+    def show_privileges(self):
+        privileges = self.privileges
+        for privilege in privileges:
+            print(f"Privilege: {privilege}.")
+
+class Admin(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.
+
+
+administrator = Admin('neal', 'orven')
+administrator.show_privileges()
+
+# Privilege: allowed to add messages.
+# Privilege: allowed to delete users.
+# Privilege: allowed to ban users.
