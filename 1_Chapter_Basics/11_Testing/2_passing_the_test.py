a1654553e969c9a84ab4a8b114493f33486c3105
Neal O <nealorven@gmail.com>
2019-04-24 00:39:46 +0600

    Update 04 24 19 00:40

--- 1_Chapter_Basics/11_Testing/2_passing_the_test.py
+++ 1_Chapter_Basics/11_Testing/2_passing_the_test.py
@@ -2,9 +2,9 @@
 # Прохождение теста
 
 import unittest
-from name_function import get_formatted_name
+#from name_function import get_formatted_name
 #from name_function_middle import get_formatted_name
-#from name_function_middle_2 import get_formatted_name
+from name_function_middle_2 import get_formatted_name
 
 
 class NamesTestCase(unittest.TestCase):
@@ -15,6 +15,11 @@ class NamesTestCase(unittest.TestCase):
         """Имена вида 'Janis Joplin' работают правильно?"""
         formatted_name = get_formatted_name('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
+
+    def test_first_last_middle_name(self):
+        """Работают ли такие имена, как 'Wolfgang Amadeus Mozart'?"""
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
 
 
 unittest.main()
--- 1_Chapter_Basics/11_Testing/name_function_middle_2.py
+++ 1_Chapter_Basics/11_Testing/name_function_middle_2.py
@@ -5,15 +5,15 @@
 def get_formatted_name(first, last, middle=''):
     """Строит отформатированное полное имя."""
     if middle:
-        full_name = first + ' ' + middle + ' ' + last
+        full_name = f"{first} {middle} {last}"
     else:
-        full_name = first + ' ' + last
+        full_name = f"{first} {last}"
     return full_name.title()
 
 # ПОЛОЖИТЕЛЬНО
 
-# .
+# ..
 # ----------------------------------------------------------------------
-# Ran 1 test in 0.000s
+# Ran 2 tests in 0.001s
 #
 # OK
