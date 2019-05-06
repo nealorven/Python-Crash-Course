3c4660610ba73fda2e8188c672973abc9ee64918
Neal O <nealorven@gmail.com>
2019-05-06 21:59:11 +0600

    Update

--- 2_Chapter_Projects/alien_invasion/game_functions.py
+++ 2_Chapter_Projects/alien_invasion/game_functions.py
@@ -2,29 +2,37 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """Реагирует на нажатие клавиш."""
+    # Переместить корабль вправо если КНОПКА ВПРАВО нажата.
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    # Переместить корабль влево если КНОПКА ВЛЕВО нажата.
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Реагирует на отпускание клавиш."""
+    # Корабль остается на месте если КНОПКА ВПРАВО НЕ нажата.
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    # Корабль остается на месте если КНОПКА ВЛЕВО НЕ нажата.
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     """Обрабатывает нажатия клавиш и события мыши."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         # Если КНОПКА нажата то...
         elif event.type == pygame.KEYDOWN:
-            # Переместить корабль вправо если КНОПКА ВПРАВО нажата.
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            # Переместить корабль влево если КНОПКА ВЛЕВО нажата.
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_events(event, ship)
         # Если КНОПКА НЕ нажата то...
         elif event.type == pygame.KEYUP:
-            # Корабль остается на месте если КНОПКА ВПРАВО НЕ нажата.
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            # Корабль остается на месте если КНОПКА ВЛЕВО НЕ нажата.
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(eve_settings, screen, ship):
